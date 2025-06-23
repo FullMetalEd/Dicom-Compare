acb6d22a337a17806f763118e3985a6c24d31416
Ed <edward.lonsdale1997@gmail.com>
2025-06-23 17:45:36 +1000

    working on packaging

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Python Dev Environment for Nixos";
+  description = "DICOM Compare Tool - Compare DICOM studies from different ZIP exports";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,31 +10,73 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        
+        dicom-compare = pkgs.python3Packages.buildPythonApplication {
+          pname = "dicom-compare";
+          version = "0.1.0";
+
+          src = ./.;
+
+          pyproject = true;
+
+          nativeBuildInputs = with pkgs.python3Packages; [
+            hatchling
+          ];
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            pydicom
+            typer
+            pandas
+            rich
+            openpyxl
+            matplotlib
+          ];
+
+          # Skip tests for now
+          doCheck = false;
+
+          meta = with pkgs.lib; {
+            description = "Compare DICOM studies from different ZIP exports to identify differences";
+            homepage = "https://github.com/yourusername/dicom-compare";
+            license = licenses.mit;
+            maintainers = [ ];
+          };
+        };
       in
       {
+        packages = {
+          default = dicom-compare;
+          dicom-compare = dicom-compare;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = dicom-compare;
+            name = "dicom-compare";  # Changed to match the script name in pyproject.toml
+          };
+          dicom-compare = flake-utils.lib.mkApp {
+            drv = dicom-compare;
+            name = "dicom-compare";  # Added this for consistency
+          };
+        };
+
         devShells.default = pkgs.mkShell {
-	  buildInputs = with pkgs; [
-	    uv
-	    gcc
-        python3Packages.numpy
-	    (python3.withPackages (ps: with ps; with python3Packages; [
-              #jupyter
-              #ipython
-            ]))
-	];
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.hatchling
+          ] ++ (with pkgs.python3Packages; [
+            pydicom
+            typer
+            pandas
+            rich
+            openpyxl
+            matplotlib
+          ]);
 
-	shellHook = ''
-	  rm -rf .venv
-	  uv venv .venv
-	  source .venv/bin/activate
-	  
-	  export PIP_REQUIRE_VIRTUALENV=true
-	  export PIP_USE_UV=1
-
-	  uv pip install -e .
-	'';
-      
-      };
-    });
+          shellHook = ''
+            echo "🏥 DICOM Compare development environment"
+            echo "Run: python3 -m dicom_compare.main --help"  # Fixed: underscore to match directory
+          '';
+        };
+      });
 }
-	
